543b05d9b2da6ab18c04f95cca466b4fe33f1ac6
wechuli Godwin <wechualigoddy@gmail.com>
2025-05-29 17:54:15 +0300

    first commit

---
+++ lib/screens/home_screan.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScrean extends StatelessWidget {
+  const HomeScrean({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
