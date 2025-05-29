09f086a4911b11bdd1275ff93a640c25739dcfbd
wechuli Godwin <wechualigoddy@gmail.com>
2025-05-29 19:14:28 +0300

    resolved sdk version conflicts

--- lib/screens/home_screan.dart
+++ lib/screens/home_screan.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScrean extends StatelessWidget {
   const HomeScrean({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarBrightness: Brightness.dark,
+        ),
+      ),
+    );
   }
 }
